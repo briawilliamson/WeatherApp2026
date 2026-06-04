17e8bd742299c43c30d4a52f56d2e67845f3caa7
briawilliamson <briawilliamson99@gmail.com>
2026-06-04 18:19:56 -0400

    added API

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ function refreshWeather(response) {
     let tempElement = document.querySelector("#temp");
     let temp = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("description");
+
+    console.log(response.data.condition.description);
 cityElement.innerHTML = response.data.city;
+descriptionElement.innerHTML = response.data.condition.description;
 tempElement.innerHTML = Math.round(temp);
+
+    let fahrenheitElement = document.querySelector("#farenheit");
+fahrenheitElement.innerHTML = "°F";
 }
 
 function searchCity(city) {
     let apiKey = "0o5be24a9etfa3a41b3e03ff5892ac86";
-    let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric';
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -20,3 +27,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Indianapolis");
